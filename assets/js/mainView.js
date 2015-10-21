9d907d031b762e95a3abb0eb391f9b0dc57a5231
Maciej Kamela <maciej.kamela@pearson.com>
2015-10-21 14:52:53 +0200

    changing poster

--- assets/js/mainView.js
+++ assets/js/mainView.js
@@ -4,27 +4,33 @@
 'use strict';
 var app = app || {};
 app.mainView = (function () {
-        return {
-            init: function () {
-                this.getAllMovies();
-            },
-            getAllMovies: function () {
-                var displayMoviesBtn = $('.display-all-movies-btn');
-                displayMoviesBtn.on('click', function () {
-                    app.apiCalls.getAllData();
-                });
-            }
-        };
-    //var path = '../_images/';
-    var movies =[ 'avangers.jpg','carlitosway.jpg', 'godfather.jpg', 'scareface.jpg', 'serpico.jpg'];
+    return {
+        init: function () {
+            this.getAllMovies();
+            this.getNextMovie();
+        },
+        properties: {
+            imgPath: './assets/_images/',
+            movies: ['avangers.jpg', 'carlitosway.jpg', 'godfather.jpg', 'scareface.jpg', 'serpico.jpg'],
+            counter: 0
+        },
+        getAllMovies: function () {
+            var displayMoviesBtn = $('.display-all-movies-btn');
+            displayMoviesBtn.on('click', function () {
+                app.apiCalls.getAllData();
+            });
+        },
+        getNextMovie: function () {
+            var nextMovieArrow = $('.poster-nav-left-arrow'),
+                mainPoster = $('.main-poster'),
+                self = this;
+            nextMovieArrow.on('click', function () {
+                self.properties.counter++;
+                mainPoster.css({backgroundImage: 'url(' + self.properties.imgPath + self.properties.movies[self.properties.counter] + ')'});
 
+            });
+        }
+    };
 
-    nextMovie: function () {
-        currentIndex++;
-
-        el.css(background, path + movie[currentIndex]);
-
-    }
-
-    }()
+}()
 );
