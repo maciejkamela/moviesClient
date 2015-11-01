ba1470c1fdaa801ec14f8be5e275cb86bf99408f
camel <camel>
2015-11-01 20:35:12 +0100

    Creating movie gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,48 @@
+'use strict';
+var app = app || {};
+app.Gallery = function ($galleryContainer, galleryCollection, galleryPath) {
+    this.$galleryContainer = $galleryContainer;
+    this.$galleryWrapper = $('<div>').addClass('row gallery-wrapper');
+    this.$galleryContainer.append(this.$galleryWrapper);
+    this.$galleryIsotop = $('<ul>').addClass('gallery-isotop');
+    this.$galleryWrapper.append(this.$galleryIsotop);
+    this.galleryCollection = galleryCollection;
+    this.galleryAmount = galleryCollection.length;
+    this.galleryPath = galleryPath;
+};
+
+app.Gallery.prototype.createGallery = function () {
+    var $galleryItem,
+        $figureItem,
+        $figcaption,
+        $galleryItemText;
+    for (var i = 0; i < this.galleryAmount; i++) {
+        $figcaption = $('<figcaption>');
+        $galleryItemText = $('<div>').addClass('gallery-item-text');
+        $galleryItem = $('<li>').addClass('col-xs-12 col-sm-6 col-md-4 gallery-item');
+        $figcaption.append($galleryItemText);
+        $figureItem = $('<figure>').addClass('figure-item').append($figcaption);
+        $figureItem.css({
+            backgroundImage: 'url(' + this.galleryPath + this.galleryCollection[i] + ')',
+            width: 100 + 'px'
+        });
+        $galleryItem.append($figureItem);
+        this.$galleryIsotop.append($galleryItem);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/js/mainView.js
+++ assets/js/mainView.js
@@ -12,23 +12,31 @@ app.mainView = (function () {
             var slider = new app.Slider(this.sliderSettings.$slider, this.sliderSettings.movies, this.sliderSettings.imgPath, this.sliderSettings.currentMovie);
             slider.createSlider();
             slider.createArrows();
+
             var poster = new app.Poster(this.posterSettings.$posterContainer, this.posterSettings.postersCollection, this.posterSettings.posterPath, this.posterSettings.rating);
             poster.createPosterContainer();
             poster.zoomMovieRating();
             poster.toggleMovieDetails();
+
+            var gallery = new app.Gallery(this.gallerySettings.$galleryContainer, this.gallerySettings.galleryCollection, this.gallerySettings.galleryPath);
+            gallery.createGallery();
         },
         sliderSettings: {
             $slider: $('.slider'),
             imgPath: './assets/_images/',
             currentMovie: 0,
-            movies: ['brasco.jpg', 'avengers.jpg', 'carlitosway.jpg', 'godfather.jpg', 'scareface.jpg', 'serpico.jpg', 'kamel.jpg', 'goodFellas.jpg']
+            movies: ['postersilence.jpg', 'avengers.jpg', 'carlitosway.jpg', 'godfather.jpg', 'scareface.jpg', 'serpico.jpg', 'kamel.jpg', 'goodFellas.jpg']
         },
         posterSettings: {
             $posterContainer: $('.recommended-movies'),
             posterPath: './assets/_posters/',
-            currentMovie: 0,
-            postersCollection: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],
+            postersCollection: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
             rating: '5.0'
+        },
+        gallerySettings: {
+            $galleryContainer: $('.gallery-container'),
+            galleryPath: './assets/_gallery/',
+            galleryCollection: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
         },
         getAllMovies: function () {
             var displayMoviesBtn = $('.display-all-movies-btn');
